f50652117c3952957b4f1779360065e7e4a5682f
Ted Lilley <ted.lilley@gmail.com>
2016-02-09 09:40:48 -0500

    sample task

---
+++ share/jiff/jiff-sample-task.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Usage: jiff sample-task --option-one [subtask]
+# Summary: A sample task template
+# Help: Here is a more full explanation.
+# It can go multiple lines
+
+source commonlib
+# source jifflib # jiff-specific functions
+# source platformlib # platform-specific functions
+
+strict_mode_on
+# trace_on # for debugging while developing
+
+# Provide jiff completions
+# if match "${1:-}" "--complete"; then
+#   cat <<EOM
+# --option-one
+# --option-two
+# subtask
+# EOM
+#   exit
+# fi
+
+usage () {
+  echo "Usage: jiff task --option-one [subtask]"
+}
+
+# subtask="${1:-}"
+
+# case "${subtask}" in
+# "one" | "two" )
+#   do_something
+#   ;;
+# * )
+#   usage
+#   ;;
+# esac
