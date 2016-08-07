74160f6d32040cbdd2dfa0f82a45703cd73448bc
Ted Lilley <ted.lilley@accelerated.com>
2016-08-06 21:46:10 -0400

    updates

--- lib/jiff.sh
+++ lib/jiff.sh
@@ -2,75 +2,77 @@
 
 # Functions specific to jiff but not platform-specific
 
-source path.sh
+source array.sh
+source file.sh
 source shell.sh
 source string.sh
-source array.sh
+source ui.sh
 
 RoleLink="${RoleLink:-${_JIFF_ROOT}/role/current}"
 LibExecDirectory="${LibExecDirectory:-${_JIFF_ROOT}/libexec}"
 ContextLink="${ContextLink:-${_JIFF_ROOT}/context/current}"
 
-jiff::current_context () {
+jiff.current_context() {
   local context
 
-  context="$( readlink "${ContextLink}" )"
-  context=( $(str::split "/" "${context}") )
-  context=( $(ary::slice "0" $((${#context[@]}-3)) "${context[@]}") )
-  context="$(ary::join "/" "${context[@]}")"
-  printf "%s" "${context}"
+  context="$( readlink "$ContextLink" )"
+  context=( $(str.split context "/") )
+  context=( $(ary.slice "0" $((${#context[@]}-3)) "${context[@]}") )
+  context="$(ary.join "/" "${context[@]}")"
+  printf "%s" "$context"
 }
 
-jiff::context_less_role () {
+jiff.context_less_role() {
   local context
 
-  context="$( readlink "${ContextLink}" )"
-  context=( $(str::split "/" "${context}") )
-  context=( $(ary::slice "0" $((${#context[@]}-2)) "${context[@]}") )
-  context="$(ary::join "/" "${context[@]}")"
-  printf "%s" "${context}"
+  context="$( fil.readlink ContextLink )"
+  context=( $(str.split "/" "$context") )
+  context=( $(ary.slice context 0 -2) )
+  context="$(ary.join context "/")"
+  printf "%s" "$context"
 }
 
-jiff::is_jiff_task () {
-  pth::is_file "${LibExecDirectory}/jiff-${1}"
+jiff.is_jiff_task() {
+  local file="${LibExecDirectory}/jiff-$1"
+  fil.exist? file
 }
 
-jiff::make_rolelinks () {
+jiff.make_rolelinks() {
   local task
   local file
 
-  for file in "${RoleLink}"/jiff-*; do
-    str::eql? file "${RoleLink}/jiff-*" && continue
-    task="$(pth::base_name "${file}")"
-    pth::make_symlink "${LibExecDirectory}/${task}" "${file}"
+  for file in "$RoleLink"/jiff-*; do
+    str.eql? file "${RoleLink}/jiff-*" && continue
+    task="$(fil.base_name file)"
+    fil.make_symlink "${LibExecDirectory}/${task}" "$file"
   done
 }
 
-jiff::run_and_exit_if_is_jiff_task () {
+jiff.run_and_exit_if_is_jiff_task() {
   local task
 
-  task="${1}"
+  task="$1"
   shift
-  ! jiff::is_jiff_task "${task}" || exec jiff "${task}" "${@}"
+  ! jiff.is_jiff_task "$task" || exec jiff "$task" "$@"
 }
 
-jiff::run_if_is_jiff_task () {
+jiff.run_if_is_jiff_task() {
   local task
 
-  task="${1}"
-  if jiff::is_jiff_task "${task}"; then
-    jiff "${task}"
+  task="$1"
+  if jiff.is_jiff_task "$task"; then
+    jiff "$task"
   fi
 }
 
-jiff::run_subtask () {
+jiff.run_subtask() {
   local verb
   local object
 
-  verb="${1}"
+  verb="$1"
   shift
   object="${1:-}"
   shift
-  sh::usage_and_exit_if_is_empty "${object}"
-  jiff::run_and_exit_if_is_jiff_task "${verb}-${object}" "${@}"
+  ui.usage_and_exit_if_blank? object
+  jiff.run_and_exit_if_is_jiff_task "${verb}-$object" "$@"
 }
